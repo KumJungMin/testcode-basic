16a86d78913290c8313f5e0984df4a711586d5cf
KumJungMin <rmawjdals@naver.com>
2024-06-08 15:01:37 +0900

    TDD(Refactor 단계) - onChange props

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -5,14 +5,17 @@ export default function TextField(props) {
   const { onChange = () => {} } = props;
 
   // 2. 테스트를 통과하는 코드를 작성한다.(실패하지 않는 테스트)
+  // 3. 리팩토링 및 중복 코드 제거(테스트 통과 여부 확인)
+  const handleChange = event => {
+    setValue(event.target.value);
+    onChange(event.target.value);
+  };
+
   return (
     <input
       type="text"
       placeholder="텍스트를 입력해 주세요."
-      onChange={event => {
-        setValue(event.target.value);
-        onChange(event.target.value);
-      }}
+      onChange={handleChange}
       value={value}
     />
   );
