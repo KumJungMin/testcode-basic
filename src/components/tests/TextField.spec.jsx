b95c0a6d25f5edc2b22db9124f1ae5931d1b09c3
KumJungMin <rmawjdals@naver.com>
2024-06-08 14:54:10 +0900

    TDD(Red 단계) - 구현하고자 하는 기능에 대한 테스트를 작성

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+// TDD 방법론
+// 1. 구현하고자 하는 기능에 대한 테스트를 작성한다.(실패하는 테스트)
+it('기본 placeholder "텍스트를 입력해 주세요."가 노출되어야 한다.', async () => {
+  await render(<TextField />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  expect(textInput).toBeInTheDocument();
 });
