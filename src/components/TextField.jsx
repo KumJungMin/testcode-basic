c45745c282a5d3d17f1ad189c39618f4d870a8e2
KumJungMin <rmawjdals@naver.com>
2024-06-08 15:00:45 +0900

    TDD(Green 단계) - onChange props

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-export default function TextField() {
+export default function TextField(props) {
+  const [value, setValue] = React.useState('');
+  const { onChange = () => {} } = props;
+
   // 2. 테스트를 통과하는 코드를 작성한다.(실패하지 않는 테스트)
-  return <input type="text" placeholder="텍스트를 입력해 주세요." />;
+  return (
+    <input
+      type="text"
+      placeholder="텍스트를 입력해 주세요."
+      onChange={event => {
+        setValue(event.target.value);
+        onChange(event.target.value);
+      }}
+      value={value}
+    />
+  );
 }
