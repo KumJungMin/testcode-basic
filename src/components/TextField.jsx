cb0f766bdbef417082dd9c38e7a088760878284a
KumJungMin <rmawjdals@naver.com>
2024-06-08 14:56:03 +0900

    TDD(Green 단계) - 테스트가 통과하도록 구현

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export default function TextField() {
-  return <input type="text" />;
+  // 2. 테스트를 통과하는 코드를 작성한다.(실패하지 않는 테스트)
+  return <input type="text" placeholder="텍스트를 입력해 주세요." />;
 }
