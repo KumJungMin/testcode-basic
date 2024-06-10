0654a01a1f379423c6d47e2880b076efc832a83b
KumJungMin <rmawjdals@naver.com>
2024-06-10 19:23:38 +0900

    스토리북 구성요소 보기

--- src/pages/home/components/stories/ProductCard.stories.jsx
+++ src/pages/home/components/stories/ProductCard.stories.jsx
@@ -1,9 +1,14 @@
 import product from '@/__mocks__/response/product.json';
 import ProductCard from '@/pages/home/components/ProductCard';
 
+// 메타 정보
+// : 스토리북에 표시될 컴포넌트의 정보를 설정
 export default {
+  // 고유 명칭, 스토리북에서 보여질 그룹명, "/"를 사용하면, 하위 그룹으로 표시됨
   title: '홈/상품 카드',
   component: ProductCard,
+
+  // argTypes: 사용자가 컴포넌트에 전달할 수 있는 인수를 설정
   argTypes: {
     product: {
       control: 'object',
@@ -12,8 +17,12 @@ export default {
   },
 };
 
+// 스토리 컴포넌트
 export const Default = {
-  name: '기본',
+  name: '기본', // 스토리북에서 보여질 스토리명
+
+  // 컴포넌트에 전달할 인수
+  // argTypes에 정의된 인수를 전달해야 함
   args: {
     product,
   },
